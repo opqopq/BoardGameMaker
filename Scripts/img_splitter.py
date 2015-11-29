1895c0bc0b7cd0c86748cd675b9de6baf85ae1be
Olivier POYEN <poyol@hotmail.com>
2015-11-29 18:55:45 +0100

    folder cleaning

--- Scripts/img_splitter.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'opq'
-
-if not file_selector.selection:
-    alert('Choose file first')
-else:
-    img_path = file_selector.selection[0]
--- Scripts/unit_test.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'HO.OPOYEN'
-
-def test_print():
-    prepare_pdf(stack, 'test.pdf')
-
-test_print()
